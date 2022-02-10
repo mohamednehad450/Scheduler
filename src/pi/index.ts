0b2603f19ae272d8af4bd998f4dbc16a2b0cc14f
Mohamed Nehad <mohammednehad550@gmail.com>
2022-02-10 04:47:52 +0200

    fix: fix validateSchedule

--- src/pi/index.ts
+++ src/pi/index.ts
@@ -5,8 +5,8 @@ export const validateSchedule = (m: Partial<Schedule>): Schedule => {
     const name = m.name || `UNNAMED SCHEDULE: (id: ${id})`
     const sched = validateScheduleData(m.sched)
     const lastRun = !m.lastRun ? undefined : new Date(m.lastRun)
-    const pins = Array.isArray(m.pins) ? m.pins : []
-    pins.map(p => {
+    let pins = Array.isArray(m.pins) ? m.pins : []
+    pins = pins.map(p => {
         const pin = validatePin({ ...p.pin, label: p.pin?.label || `UNNAMED PIN: (channel: ${p.pin?.channel})` })
         const duration = validateDuration(p.duration)
         const offset = validateDuration(p.offset)
