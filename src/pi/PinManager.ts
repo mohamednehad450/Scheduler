c5ce8d13449b9c8ab3464e720e731d5e93934494
Mohamed Nehad <mohammednehad550@gmail.com>
2022-02-10 04:47:31 +0200

    fix: Fix file name

---
+++ src/pi/PinManager.ts
@@ -0,0 +1,112 @@
+import { AppDB } from "../db";
+import { Gpio, GpioConfig } from "./gpio";
+import { ID, Pin, validatePin } from "./utils";
+
+type CallBack<T> = (err: Error | null | undefined, v?: T) => void
+
+interface GpioManager {
+    isRunning: (id: ID, cb: CallBack<boolean>) => void
+    run: (id: ID, cb: CallBack<void>) => void
+    stop: (id: ID, cb: CallBack<void>) => void
+    pinsStatus: (cb: CallBack<{ p: Pin, running: boolean, err: Error | null | undefined }[]>) => void
+    rest: (cb: CallBack<void>) => void
+}
+
+const OFF = true
+const ON = false
+
+class PinManager implements GpioManager {
+
+    gpio: Gpio
+    config: GpioConfig
+    db: AppDB['pinsDb']
+
+    constructor(gpio: Gpio, config: GpioConfig, db: AppDB['pinsDb']) {
+
+        this.config = config
+        this.gpio = gpio
+        this.db = db
+
+        const { boardMode } = this.config
+
+        this.db.list((err, pins) => {
+            if (err) {
+                //TODO
+                return
+            }
+            this.gpio.setMode(boardMode)
+            pins?.forEach((p) => gpio.setup(p.channel, gpio.DIR_HIGH))
+        })
+
+
+    }
+
+
+
+    isRunning = (id: ID, cb: CallBack<boolean>) => {
+        this.db.get(id, (err, p) => {
+            if (err) {
+                cb(err)
+                return
+            }
+            p && this.gpio.read(p.channel, (err, high) => cb(err, !high))
+        })
+    };
+
+    run = (id: ID, cb: CallBack<void>) => {
+        this.db.get(id, (err, p) => {
+            if (err) {
+                cb(err)
+                return
+            }
+            p && this.gpio.write(p.channel, ON, cb)
+        })
+    }
+
+    stop = (id: ID, cb: CallBack<void>) => {
+        this.db.get(id, (err, p) => {
+            if (err) {
+                cb(err)
+                return
+            }
+            p && this.gpio.write(p.channel, OFF, cb)
+        })
+    };
+
+    pinsStatus = (cb: CallBack<{ p: Pin, running: boolean, err: Error | null | undefined }[]>) => {
+        const status: {
+            p: Pin,
+            running: boolean
+            err: Error | null | undefined
+        }[] = []
+        this.db.list((err, pins) => {
+            pins && pins.forEach((p) => {
+                this.isRunning(p.id, (err, running) => {
+                    err ?
+                        status.push({ p, running: false, err: err }) :
+                        status.push({ p, running: !!running, err: null })
+                    status.length === pins.length && cb(null, status)
+                })
+            })
+        })
+    };
+
+    rest = (cb: CallBack<void>) => {
+        this.gpio.destroy((err) => {
+            if (err) {
+                cb(err)
+                return
+            }
+            this.db.list((err, pins) => {
+                if (err) {
+                    cb(err)
+                    return
+                }
+                this.gpio.setMode(this.config.boardMode)
+                pins && pins.forEach(p => this.gpio.setup(p.channel, this.gpio.DIR_HIGH))
+            })
+        })
+    };
+}
+
+export default PinManager
