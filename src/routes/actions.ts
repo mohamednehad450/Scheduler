c835919beec1306c1d5b9c5270aa35ce1a7632a6
Mohamed Nehad <mohammednehad550@gmail.com>
2022-02-10 05:19:46 +0200

    feat: Implement actions

--- src/routes/actions.ts
+++ src/routes/actions.ts
@@ -1,47 +1,83 @@
 import { Express } from 'express'
 import { AppDB } from '../db'
+import gpio, { config } from '../pi/gpio'
+import PinManager from '../pi/PinManager'
+import Scheduler from '../pi/Scheduler'
 
 
 const routes = {
-    RUN: "actions/run",
-    ACTIVATE: 'actions/activate',
+    RUN: "/actions/run",
+    ACTIVATE: '/actions/activate',
 }
 
 
 export default (app: Express, db: AppDB) => {
 
+    const pinManager = new PinManager(gpio, config, db.pinsDb)
+    const scheduler = new Scheduler(db, pinManager)
+
     // Run schedule
     app.post(routes.RUN, (req, res) => {
-
+        scheduler.run(req.body.id, (err) => {
+            if (err) {
+                res.status(400)
+            }
+            res.send()
+        })
     })
 
 
     // Stop schedule
     app.delete(routes.RUN, (req, res) => {
-
+        scheduler.stop(req.body.id || 'EMPTY_ID', (err) => {
+            if (err) {
+                res.status(400)
+            }
+            res.send()
+        })
     })
 
 
     // Running schedules
     app.get(routes.RUN, (req, res) => {
-
+        scheduler.running((err, ids) => {
+            if (err) {
+                res.status(500)
+            }
+            res.json(ids || [])
+        })
     })
 
 
-    // Acivate schedule
+    // Activate schedule
     app.post(routes.ACTIVATE, (req, res) => {
-
+        scheduler.activate(req.body.id || 'EMPTY_ID', (err) => {
+            if (err) {
+                res.status(400)
+            }
+            res.send()
+        })
     })
 
 
     // Deactivate schedule
     app.delete(routes.ACTIVATE, (req, res) => {
-
+        scheduler.deactivate(req.body.id || 'EMPTY_ID', (err) => {
+            if (err) {
+                res.status(400)
+            }
+            res.send()
+        })
     })
 
 
     // Active schedules
     app.get(routes.ACTIVATE, (req, res) => {
-
+        scheduler.activeSchedules((err, ids) => {
+            if (err) {
+                res.status(500)
+            }
+            res.json(ids || [])
+        })
     })
 }
