bb0ee7fe4cbeaef7fd945ffeeeaee978937bfa23
Mohamed Nehad <mohammednehad550@gmail.com>
2023-03-01 01:12:25 +0200

    fix: db -> misc

--- src/db/misc.ts
+++ src/db/misc.ts
@@ -1,3 +1,4 @@
+import EventEmitter from "events"
 import { ObjectSchema } from "joi"
 
 type Predict<T> = (item: T) => boolean
@@ -7,6 +8,18 @@ type Pagination = {
     perPage?: number,
     sort?: Compare<unknown>
 }
+type PageInfo = {
+    total: number,
+    current: number,
+    perPage: number,
+}
+type ObjectValidators<T> = {
+    loadValidator?: ObjectSchema<T>
+    inputValidator?: ObjectSchema<T>
+    updateValidator?: ObjectSchema<T>
+}
+
+
 interface Db<K, T> {
     insert: (obj: T) => Promise<T>
     update: (id: K, obj: Partial<T>) => Promise<T | undefined>
@@ -20,10 +33,36 @@ interface Db<K, T> {
     countBy: (predict: Predict<T>) => Promise<number>
 }
 
-type ObjectValidators<T> = {
-    loadValidator?: ObjectSchema<T>
-    inputValidator?: ObjectSchema<T>
-    updateValidator?: ObjectSchema<T>
+interface DbSync<K, T> {
+    insert: (obj: T) => T
+    update: (id: K, obj: Partial<T>) => T | undefined
+    findByKey: (key: K) => T | undefined
+    findBy: (predict: Predict<T>, pagination?: Pagination) => T[]
+    findAll: (pagination?: Pagination) => T[]
+    deleteBy: (predict: Predict<T>) => void
+    deleteByKey: (key: K) => void
+    deleteAll: () => void
+    count: () => number
+    countBy: (predict: Predict<T>) => number
+}
+
+interface CRUD<K, T> extends EventEmitter {
+    insert: (obj: any) => Promise<T>
+    get: (id: K,) => Promise<T | undefined>
+    set: (id: K, obj: any) => Promise<T | undefined>
+    update: (id: K, obj: any,) => Promise<T | undefined>
+    remove: (id: K) => Promise<void>
+    list: (pagination?: Pagination) => Promise<T[]>
+}
+
+interface EventsCRUD<K, T> {
+    emit: (obj: T) => Promise<T>
+    get: (key: K) => Promise<T | null>
+    remove: (key: K) => Promise<void>
+    removeByEmitter: (emitterKey: any) => Promise<void>
+    removeAll: () => Promise<void>
+    listAll: (pagination?: Pagination) => Promise<{ events: T[], pageInfo: PageInfo }>
+    listByEmitter: (emitterKey: any, pagination?: Pagination) => Promise<{ events: T[], pageInfo: PageInfo }>
 }
 
 
@@ -32,5 +71,9 @@ export type {
     Predict,
     Compare,
     Pagination,
+    PageInfo,
     Db,
+    DbSync,
+    CRUD,
+    EventsCRUD,
 }
