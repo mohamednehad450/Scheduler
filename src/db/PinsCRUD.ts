f0bc89ad18d2b2e4c336c1891c22bae6453729a8
Mohamed Nehad <mohammednehad550@gmail.com>
2023-03-02 13:51:57 +0200

    fix: Update sequences on pin delete and update

--- src/db/PinsCRUD.ts
+++ src/db/PinsCRUD.ts
@@ -2,21 +2,59 @@
 import EventEmitter from "events";
 import JSONDb from "./JSONDb";
 import { CRUD } from "./misc";
-import { Pin } from "./types";
+import SequenceCRUD from "./SequenceCRUD";
+import { BaseSequence, Pin } from "./types";
 
 export default class PinCRUD extends EventEmitter implements CRUD<Pin['channel'], Pin> {
 
     db: JSONDb<Pin['channel'], Pin>
+    sequenceCRUD: SequenceCRUD
 
-    constructor(db: JSONDb<Pin['channel'], Pin>) {
+    constructor(
+        db: JSONDb<Pin['channel'], Pin>,
+        sequenceCRUD: SequenceCRUD
+    ) {
         super()
         this.db = db
+        this.sequenceCRUD = sequenceCRUD
     }
 
     insert = (obj: Pin) => this.db.insert(obj)
-    update = (id: Pin['channel'], obj: Partial<Pin>) => this.db.update(id, obj)
+    update = async (id: Pin['channel'], obj: Partial<Pin>) => {
+        const newPin = await this.db.update(id, obj)
+        if (!newPin) return
+
+        if (id === newPin.channel) return newPin
+
+        // Update changed channels
+        await this.sequenceCRUD.db.updateBy(
+            _ => true,
+            sequence => ({
+                ...sequence,
+                orders: sequence.orders
+                    .map(o =>
+                        o.channel === id ?
+                            ({ ...o, channel: newPin.channel }) :
+                            o
+                    )
+            })
+        )
+        return newPin
+    }
+
     set = this.update
-    remove = (key: Pin['channel']) => this.db.deleteByKey(key)
+    remove = async (key: Pin['channel']) => {
+        await this.db.deleteByKey(key)
+        const updated = await this.sequenceCRUD.db.updateBy(
+            _ => true,
+            s => ({ ...s, orders: s.orders.filter(o => o.channel !== key) })
+        )
+        await Promise.all(
+            updated
+                .filter(s => !s.orders.length)
+                .map(s => this.sequenceCRUD.remove(s.id))
+        )
+    }
     get = (key: Pin['channel']) => this.db.findByKey(key)
     list = () => this.db.findAll()
 }
