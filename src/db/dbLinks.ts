9667d7c50686b010d84aa2f2737caa62bd0bcee0
Mohamed Nehad <mohammednehad550@gmail.com>
2023-03-03 23:35:28 +0200

    feat: Add dbLinks

---
+++ src/db/dbLinks.ts
@@ -0,0 +1,52 @@
+import { Db, ForeignDbLink } from "./misc"
+import { BaseCron, BaseSequence, BaseSequenceEvent, CronSequence, Pin } from "./types"
+
+
+type DbLinker<K, T, FK, FT> = (db: Db<FK, FT>) => ForeignDbLink<K, T, FK, FT>
+
+
+const pinSequenceLink: DbLinker<Pin['channel'], Pin, BaseSequence['id'], BaseSequence> = (db) => ({
+    db,
+    onDelete: (sequence, channel) => ({
+        ...sequence,
+        orders: sequence.orders.filter(o => o.channel === channel)
+    }),
+    onUpdate: (sequence, { channel }, oldChannel) => {
+        if (!oldChannel) return sequence
+        return {
+            ...sequence,
+            orders: sequence.orders.map(o => o.channel === oldChannel ? { ...o, channel, } : o)
+        }
+    },
+    predict: ({ foreignItem: sequence, key: channel, oldKey: oldChannel }) =>
+        sequence.orders.some(o => o.channel === (oldChannel || channel))
+})
+
+const sequenceEventLink: DbLinker<BaseSequence['id'], BaseSequence, BaseSequenceEvent['id'], BaseSequenceEvent> = (db) => ({
+    db,
+    predict: ({ foreignItem, key, oldKey }) => foreignItem.sequenceId === (oldKey || key),
+    onDelete: "CASCADE",
+    onUpdate: (event, sequence, oldKey) => oldKey ? ({ ...event, sequenceId: sequence.id }) : event
+})
+
+const cronCSLink: DbLinker<BaseCron['id'], BaseCron, void, CronSequence> = (db) => ({
+    db,
+    predict: ({ foreignItem, key, oldKey }) => foreignItem.cronId === (oldKey || key),
+    onDelete: "CASCADE",
+    onUpdate: (cs, cron, oldKey) => oldKey ? ({ ...cs, cronId: cron.id }) : cs
+})
+
+const sequenceCSLink: DbLinker<BaseSequence['id'], BaseSequence, void, CronSequence> = (db) => ({
+    db,
+    predict: ({ foreignItem, key, oldKey }) => foreignItem.sequenceId === (oldKey || key),
+    onDelete: "CASCADE",
+    onUpdate: (cs, sequence, oldKey) => oldKey ? ({ ...cs, sequenceId: sequence.id }) : cs
+})
+
+
+export {
+    pinSequenceLink,
+    sequenceEventLink,
+    cronCSLink,
+    sequenceCSLink
+}
