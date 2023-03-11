e9494046319e84134acabe360ff12c327a1b9df3
Mohamed Nehad <mohammednehad550@gmail.com>
2023-03-11 23:43:54 +0200

    fix: Fix foreignKeysValidators

--- src/db/foreignKeysValidators.ts
+++ src/db/foreignKeysValidators.ts
@@ -2,30 +2,27 @@ import JSONDb from "./JSONDb"
 import { BaseCron, BaseSequence, Pin } from "./types"
 
 
-type ForeignKeyValidator<K, T, FK> = (db: JSONDb<K, T>) => (fk: FK) => Promise<void>
+type ForeignKeyValidator<K, T, FK> = (db: JSONDb<K, T>) => (fk: FK) => void
 
-const channelsExistsValidator: ForeignKeyValidator<Pin['channel'], Pin, Set<Pin['channel']>> = (db) => async (channels) => {
-    const pins = await Promise.all([...channels].map(channel => {
-        const a = async () => {
-            return {
-                exists: await db.exists(channel),
-                channel
-            }
-        }
-        return a()
+const channelsExistsValidator: ForeignKeyValidator<Pin['channel'], Pin, Set<Pin['channel']>> = (db) => (channels) => {
+    const pins = Array.from(channels).map(channel => ({
+        exists: db.exists(channel),
+        channel
     }))
-    if (pins.some(p => !p.exists)) {
-        throw new Error(`Channels: [${pins.filter(p => !p.exists).map(p => p.channel).join(", ")}] are not defined`)
+        .filter(pin => !pin.exists)
+
+    if (pins.length) {
+        throw new Error(`Channels: [${pins.map(p => p.channel).join(", ")}] are not defined`)
     }
 }
 
-const sequenceExistsValidator: ForeignKeyValidator<BaseSequence['id'], BaseSequence, BaseSequence['id']> = (db) => async (sequenceId) => {
-    if (!await db.exists(sequenceId))
+const sequenceExistsValidator: ForeignKeyValidator<BaseSequence['id'], BaseSequence, BaseSequence['id']> = (db) => (sequenceId) => {
+    if (!db.exists(sequenceId))
         throw new Error(`Sequence: ${sequenceId} doesn't exist.`)
 }
 
-const cronExistsValidator: ForeignKeyValidator<BaseCron['id'], BaseCron, BaseCron['id']> = (db) => async (cronId) => {
-    if (!await db.exists(cronId))
+const cronExistsValidator: ForeignKeyValidator<BaseCron['id'], BaseCron, BaseCron['id']> = (db) => (cronId) => {
+    if (!db.exists(cronId))
         throw new Error(`Cron: ${cronId} doesn't exist.`)
 }
 
