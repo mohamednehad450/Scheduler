2e4e4a96a4350a5ee2d5bcf4fe747f3c55004507
Mohamed Nehad <mohammednehad550@gmail.com>
2023-03-12 04:48:47 +0200

    feat: Add SchedulerIO

---
+++ src/routes/SchedulerIO.ts
@@ -0,0 +1,134 @@
+import { AppDB } from '../db'
+import { Scheduler } from '../pi'
+import { Server, Socket } from 'socket.io'
+import { verify } from 'jsonwebtoken'
+
+
+enum ACTIONS {
+    RUN = "run",
+    STOP = 'stop',
+    RESET = "reset",
+    STATE = "state",
+    TICK = "tick"
+}
+
+
+const addAction = (a: ACTIONS, func: (...args: any) => Promise<any>, socket: Socket) => {
+    socket.on(a, (actionId, ...args) => {
+        func(...args)
+            .then(() => actionId && socket.emit(actionId, true, null))
+            .catch(err => actionId && socket.emit(actionId, false, err))
+    })
+}
+
+export default async (io: Server, db: AppDB) => {
+
+    const scheduler = new Scheduler(db)
+
+    await scheduler.start()
+
+    async function getState() {
+        return {
+            runningSequences: scheduler.running(),
+            reservedPins: scheduler.getReservedPins(),
+            channelsStatus: await scheduler.channelsStatus(),
+        }
+    }
+
+    io.use((socket, next) => {
+        try {
+            const token = socket.handshake.auth.token
+            verify(token, process.env.TOKEN_KEY || '')
+            next()
+        } catch (error) {
+            next(new Error('invalid token'))
+        }
+    })
+
+    io.on('connection', (socket) => {
+        console.log('Socket Connected.')
+        console.log(`Socket ID: ${socket.id}`)
+
+        // Send time every TICK
+        const TICK = 1000
+        let tick: NodeJS.Timeout
+        const tickHandler = (cb: (d: Date) => void) => {
+            const d = new Date()
+            cb(d)
+            tick = setTimeout(() => tickHandler(cb), TICK - (d.getTime() % TICK))
+        }
+
+
+        // On channel change: update state and emit pinChange 
+        const channelChange = async (...args: any) => {
+            socket.emit('channelChange', ...args)
+        }
+
+
+        // On sequence stop: update state and emit stop 
+        const stop = async (...args: any) => {
+            socket.emit('stop', ...args)
+            socket.emit('state', {
+                reservedPins: scheduler.getReservedPins(),
+                runningSequences: scheduler.running(),
+            })
+        }
+
+
+        // On sequence finish: update state and emit stop 
+        const finish = async (...args: any) => {
+            socket.emit('finish', ...args)
+            socket.emit('state', {
+                reservedPins: scheduler.getReservedPins(),
+                runningSequences: scheduler.running(),
+            })
+        }
+
+
+        // On sequence run: update state and emit run 
+        const run = (...args: any) => {
+            socket.emit('run', ...args)
+            socket.emit('state', {
+                runningSequences: scheduler.running(),
+                reservedPins: scheduler.getReservedPins(),
+            })
+        }
+
+
+        async function sendState() {
+            socket.emit('state', await getState())
+        }
+
+        async function onTick(on: boolean) {
+            if (on) {
+                clearTimeout(tick)
+                tickHandler((d) => socket.emit('tick', d))
+            } else clearTimeout(tick)
+        }
+
+
+
+        addAction(ACTIONS.RUN, scheduler.run, socket)
+        addAction(ACTIONS.STOP, scheduler.stop, socket)
+        addAction(ACTIONS.RESET, scheduler.resetPinManager, socket)
+        addAction(ACTIONS.STATE, sendState, socket)
+        addAction(ACTIONS.TICK, onTick, socket)
+
+        scheduler.on('channelChange', channelChange)
+        scheduler.on('stop', stop)
+        scheduler.on('run', run)
+        scheduler.on('finish', finish)
+
+
+        socket.on('disconnect', () => {
+            scheduler.removeListener('channelChange', channelChange)
+            scheduler.removeListener('run', run)
+            scheduler.removeListener('stop', stop)
+            scheduler.removeListener('finish', finish)
+            socket.removeAllListeners()
+            clearTimeout(tick)
+        })
+    })
+
+    return scheduler
+}
