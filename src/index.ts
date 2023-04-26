c8bf2eeb6f450ea98883205213eb5773f1f090b6
Mohamed Nehad <mohammednehad550@gmail.com>
2023-04-27 01:20:18 +0200

    feat: Use DATABASE_FOLDER env

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,16 @@ import routes from "./routes";
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import { existsSync, mkdirSync } from "node:fs";
+import { join } from "node:path";
 
-const sqlite = Database("sqlite.db");
+const dbFolder = process.env["DATABASE_FOLDER"] || "database";
+
+if (!existsSync(dbFolder)) {
+  mkdirSync(dbFolder);
+}
+
+const sqlite = Database(join(dbFolder, "sqlite.db"));
 const db = drizzle(sqlite);
 
 migrate(db, {
